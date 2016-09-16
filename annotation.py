ed50373ef76802f98d6cce340691097ae680c01a
Yakir Abraham Reshef <yar2@loge.orchestra>
2016-09-16 16:22:35 -0400

    reconciled_to

--- annotation.py
+++ annotation.py
@@ -2,7 +2,82 @@ from __future__ import print_function, division
 import os
 import numpy as np
 import pandas as pd
+import itertools
 from pyutils import memo, fs
+
+
+# complementary bases
+COMPLEMENT = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+# bases
+BASES = COMPLEMENT.keys()
+# true iff strand ambiguous
+STRAND_AMBIGUOUS = {''.join(x): x[0] == COMPLEMENT[x[1]]
+        for x in itertools.product(BASES, BASES)
+        if x[0] != x[1]}
+# SNPS we want to keep (pairs of alleles)
+VALID_SNPS = {x for x in map(lambda y: ''.join(y), itertools.product(BASES, BASES))
+        if x[0] != x[1] and not STRAND_AMBIGUOUS[x]}
+# T iff SNP 1 has the same alleles as SNP 2 (allowing for strand or ref allele flip).
+MATCH_ALLELES = {
+        x for x in map(lambda y: ''.join(y), itertools.product(VALID_SNPS, VALID_SNPS))
+        # strand and ref match
+        if ((x[0] == x[2]) and (x[1] == x[3])) or
+        # ref match, strand flip
+        ((x[0] == COMPLEMENT[x[2]]) and (x[1] == COMPLEMENT[x[3]])) or
+        # ref flip, strand match
+        ((x[0] == x[3]) and (x[1] == x[2])) or
+        # ref flip, strand flip
+        ((x[0] == COMPLEMENT[x[3]]) and (x[1] == COMPLEMENT[x[2]]))}
+# T iff SNP 1 has the same alleles as SNP 2 w/ ref allele flip.
+FLIP_ALLELES = {''.join(x):
+        # ref flip, strand match
+        ((x[0] == x[3]) and (x[1] == x[2])) or
+        # ref flip, strand flip
+        ((x[0] == COMPLEMENT[x[3]]) and (x[1] == COMPLEMENT[x[2]]))
+        for x in MATCH_ALLELES}
+
+# Checks if SNP columns are equal. If so, saves time by using concat instead of merge.
+def smart_merge(x, y, how='inner'):
+    if len(x) == len(y) and (x.SNP == y.SNP).all():
+        x = x.reset_index(drop=True)
+        y = y.reset_index(drop=True).drop('SNP', axis=1)
+        out = pd.concat([x, y], axis=1)
+    else:
+        out = pd.merge(x, y, how=how, on='SNP')
+    return out
+
+# keeps only snps in ref, flips alleles in df to match those in ref, and sets value at
+# non-matching snps to a missing_val (0 by default). 
+def reconciled_to(ref, df, colnames, signed=True, missing_val=0):
+    result = smart_merge(ref[['CHR','BP','SNP','CM','A1','A2']],
+        df[['SNP','A1','A2']+list(colnames)].rename(columns={'A1':'A1_df','A2':'A2_df'}),
+        how='left')
+
+    # snps in ref but not in df
+    missing = result.A1_df.isnull()
+    result.loc[missing,'A1_df'] = result.loc[missing,'A1']
+    result.loc[missing,'A2_df'] = result.loc[missing,'A2']
+    result.loc[missing,colnames] = missing_val
+
+    # assign to zero SNPs in ref whose alleles don't match df
+    a1234 = (result.A1+result.A2+result.A1_df+result.A2_df).apply(lambda y: y.upper())
+    match = a1234.apply(lambda y: y in MATCH_ALLELES)
+    n_mismatch = (~match).sum()
+    print('of', len(match), 'snps,', n_mismatch, 'snps do not have same alleles,')
+    result.loc[~match,colnames] = missing_val
+    result.loc[~match,'A1_df'] = result.loc[~match,'A1']
+    result.loc[~match,'A2_df'] = result.loc[~match,'A2']
+
+    if signed:
+        a1234 = (result.A1+result.A2+result.A1_df+result.A2_df).apply(lambda y: y.upper())
+        a1234[~match] = list(MATCH_ALLELES)[0] # in case ref has strand-ambiguous alleles
+        flip = a1234.apply(lambda y: FLIP_ALLELES[y])
+        n_flip = flip.sum()
+        print('of', len(match), 'snps,', n_flip, 'snps need flipping')
+        result.loc[flip,colnames] *= -1
+
+    return result.drop(['A1_df', 'A2_df'], axis=1)
+
 
 class Annotation(object):
     def __init__(self, stem_chr, signed=True):
@@ -82,3 +157,13 @@ if __name__ == '__main__':
     print(a.sizes(22))
     print(a.total_sqnorms([1,22]))
     print(a.total_sizes([1,22]))
+
+    ref = pd.read_csv('/groups/price/yakir/data/simannot/1000G3.wim5unm/mock/22.sannot.gz',
+            delimiter='\t').drop('ANNOT', axis=1).iloc[:20]
+
+    ref.ix[0,'A1'] = a.sannot_df(22).ix[0,'A2']
+    ref.ix[0,'A2'] = a.sannot_df(22).ix[0,'A1']
+    ref.ix[1,'A1'] = 'A'; ref.ix[1,'A2'] = 'C'
+    print(ref)
+
+    print(reconciled_to(ref, a.sannot_df(22), ['ANNOT']))
