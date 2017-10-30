9ec090a4cd88a86b028a53e66fa45c023558b5ca
Reshef, Yakir <yakir@seas.harvard.edu>
2017-10-30 17:41:12 -0400

    added ypy dependency

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 setup(name='gprim',
-    version='0.1',
+    version='0.21',
     description='Utility functions for working with statistical genetic data',
     url='http://github.com/yakirr/ypy',
     author='Yakir Reshef',
     author_email='yakir@seas.harvard.edu',
     license='MIT',
     packages=['gprim'],
-    install_requires=['pysnptools','pybedtools'],
-    dependency_links=['http://github.com/yakirr/ypy/tarball/master#egg=package-0.1'],
+    install_requires=['pysnptools','pybedtools','ypy'],
     zip_safe=False)
